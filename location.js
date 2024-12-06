d59f6b6176cd7bdf35f4736281b1ed5f6664677a
IdeaCannon <161007927+IdeaCannon@users.noreply.github.com>
2024-12-06 20:23:49 +0900

    Update location.js

--- location.js
+++ location.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         주례열린 서가위치 출력
 // @namespace    juryeopenlibrary
-// @version      2024-09-15
+// @version      2024-12-06
 // @description  도서관 서가위치&지도 표시
 // @author       IdeaCannon(Assistant by HideD)
 // @match        https://library.busan.go.kr/juryebooks/book/search/bookPrint
@@ -9,6 +9,8 @@
 // @grant        none
 // @run-at       document-start
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @updateURL    https://raw.githubusercontent.com/IdeaCannon/juryelibrary.io/refs/heads/main/location.js
+// @downloadURL  https://raw.githubusercontent.com/IdeaCannon/juryelibrary.io/refs/heads/main/location.js
 // ==/UserScript==
 
 (function() {
